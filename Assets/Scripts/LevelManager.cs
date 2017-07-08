00f2f26d67118dfb40a70f616bb511651669e410
Mike Boice <mike.boice@netapp.com>
2017-07-08 14:42:30 -0600

    Add menu system

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public void loadLevel(string name)
+	{
+		Debug.Log("Level load requested for:" + name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void quitRequest()
+	{
+		Debug.Log("I want to quit");
+		Application.Quit();
+	}
+
+}

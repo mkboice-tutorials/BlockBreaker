47a990bceef429f866c5083a131483cd49ba822c
Mike Boice <mike.boice@netapp.com>
2017-07-09 14:30:12 -0600

    Add music player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    public static MusicPlayer instance = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player self destructing");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+            print("Creating music player");
+        }
+            
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+		
+    }
+}
